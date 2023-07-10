4fc55b943a37f2f610b271ebe7146dd0e2f3992a
Anna Haratova <black-isida@yandex.ru>
2023-07-10 21:51:30 +0300

    init

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # movies-explorer-frontend
 
 Макет: https://disk.yandex.ee/d/v2TFiGxLw0UqUw
+
+Pull request: https://github.com/AnnaKharatova/movies-explorer-frontend/pull/2
+
+Сайт: 
+
+фронт - movies-express.nomoredomains.work
+бэк - api.movies-express.nomoredomains.rocks
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,10 @@
 
 export const pathname = window.location.pathname;
 
+//export const BASE_URL = 'http://localhost:3000';
+
+export const BASE_URL = 'https://api.movies-express.nomoredomains.rocks/';
+
 export const nameRegex = /^[a-zA-Zа-яА-Я\s-]+$/;
+
 //export const emailRegex = /^[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]{2,}$/;
