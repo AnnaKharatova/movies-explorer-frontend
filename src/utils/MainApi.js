450be56375af3811dbf2f34c7bfef688f1a30265
Anna Haratova <black-isida@yandex.ru>
2023-07-11 09:53:00 +0300

    cors

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -11,6 +11,7 @@ export const register = (name, email, password) => {
   return fetch(`${BASE_URL}/signup`, {
     method: 'POST',
     headers: {
+      'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({ name, password, email })
@@ -21,6 +22,7 @@ export const authorize = (email, password) => {
   return fetch(`${BASE_URL}/signin`, {
     method: 'POST',
     headers: {
+      'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({ email, password })
